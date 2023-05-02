6231351256d3354077f74a0324fa3c8a7535f6ae
SamanthaJoy13 <tavaresjsamantha52@icloud.com>
2023-05-02 13:46:41 -0700

    updated config and db folders

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require ('dotenv').config ();
 
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'empTracker',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 }).promise();
 
 module.exports = connection;
--- db/schemas.sql
+++ db/schemas.sql
@@ -4,23 +4,26 @@ CREATE DATABASE empTracker;
 USE empTracker;
 
 CREATE TABLE department ( 
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    idD INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (idD)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
-    FOREIGN KEY (department_id) REFRENCES department(id)
+    idR INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY (idR),
+    FOREIGN KEY (department_id) REFERENCES department(idD)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
-    FOREIGN KEY (role_id) REFRENCES role(id)
+    idE INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (idE),
+    FOREIGN KEY (role_id) REFERENCES role(idR)
 );
